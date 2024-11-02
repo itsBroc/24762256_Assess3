8c6f374f848375efafe2900d3447f6c30d743c3c
Broc Geronimi <bg17roxs@hotmail.com>
2024-11-02 23:09:10 +1100

    updated movement with raycast.

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class PacStudentController : MonoBehaviour
 {
     public float moveDuration = 0.5f;
-    private float tileSize = 1.0f;
+    private float tileSize = 0.1f;
     private Animator pacStudentAnimator;
     public GameObject pacStudent;
     private Tweener tweener;
@@ -16,32 +16,12 @@ public class PacStudentController : MonoBehaviour
     private AudioClip eatPelletSound;
     private AudioClip moveSound;
 
+
     private KeyCode lastInput;
     private KeyCode currentInput;
     private Vector3 lastPosition;
     //private int currentX = 1;
     //private int currentY = 1;
-
-
-    public int[,] levelMap =
-    {
-        {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
-        {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
-        {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
-        {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
-        {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
-        {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
-        {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
-        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
-        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
-        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
-        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
-        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
-        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
-        {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
-        {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
-    };
-    private int[,] totalLevelMap;
 
 
 
@@ -56,7 +36,6 @@ public class PacStudentController : MonoBehaviour
         Camera.main.orthographic = true;
         Camera.main.orthographicSize = 48f;
         
-        totalLevelMap = GenerateFullGrid(levelMap);
     }
 
     // Update is called once per frame
@@ -68,9 +47,8 @@ public class PacStudentController : MonoBehaviour
         {
             TryMovePacStudent();
         }
-        Debug.Log(lastInput);
 
-        //PlayMoveAnimation();
+        PlayMoveAnimation();
     }
 
     private void HandleKey()
@@ -86,12 +64,33 @@ public class PacStudentController : MonoBehaviour
         Vector3 direction = GetDirection(lastInput);
         Vector3 positionToGetTo = pacStudent.transform.position + direction;
 
-        //if (IsMoveable(positionToGetTo))
-        //{
+        if (IsMoveable(direction))
+        {
             currentInput = lastInput;
             //bool isEatingPellet = CheckForPellet(positionToGetTo);
             StartMovement(positionToGetTo);
-        //}
+        }
+
+        if (Input.GetKey(KeyCode.W) && IsMoveable(Vector3.up))
+        {
+            currentInput = KeyCode.W;
+            StartMovement(pacStudent.transform.position + Vector3.up);
+        }
+        else if (Input.GetKey(KeyCode.A) && IsMoveable(Vector3.left))
+        {
+            currentInput = KeyCode.A;
+            StartMovement(pacStudent.transform.position + Vector3.left);
+        }
+        else if (Input.GetKey(KeyCode.S) && IsMoveable(Vector3.down))
+        {
+            currentInput = KeyCode.S;
+            StartMovement(pacStudent.transform.position + Vector3.down);
+        }
+        else if (Input.GetKey(KeyCode.D) && IsMoveable(Vector3.right))
+        {
+            currentInput = KeyCode.D;
+            StartMovement(pacStudent.transform.position + Vector3.right);
+        }
     }
 
     private Vector3 GetDirection(KeyCode input)
@@ -106,36 +105,26 @@ public class PacStudentController : MonoBehaviour
         }
     }
 
-    private bool IsMoveable(Vector3 position)
+    private bool IsMoveable(Vector3 direction)
     {
-        int gridX = Mathf.RoundToInt(position.x / tileSize);
-        int gridY = Mathf.RoundToInt(position.y / tileSize);
+        Vector3 rayOrigin = pacStudent.transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, direction, tileSize);
 
-        if(gridX < 0 || gridX >= totalLevelMap.GetLength(0) || gridY < 0 || gridY >= totalLevelMap.GetLength(1))
+        if(hit.collider != null)
         {
-            return false;
+            if (hit.collider.CompareTag("Obstacle"))
+            {
+                return false;
+            }
         }
-        int tileType = totalLevelMap[gridX, gridY];
-        return tileType == 0 || tileType == 5 || tileType == 6;
+
+        return true;
     }
 
-    private bool CheckForPellet(Vector3 position)
+    /*private bool CheckForPellet(Vector3 position)
     {
-        int gridX = Mathf.RoundToInt(position.x / tileSize);
-        int gridY = Mathf.RoundToInt(position.y / tileSize);
 
-        if (gridX < 0 || gridX >= totalLevelMap.GetLength(0) || gridY < 0 || gridY >= totalLevelMap.GetLength(1))
-        {
-            return false;
-        }
-
-        int tileType = totalLevelMap[gridX, gridY];
-        if(tileType == 5 || tileType == 6)
-        {
-            return true;
-        }
-        return false;
-    }
+    }*/
 
     private void StartMovement(Vector3 positionToGetTo)
     {
@@ -143,25 +132,11 @@ public class PacStudentController : MonoBehaviour
         lastPosition = pacStudent.transform.position;
     }
 
-    /*bool CheckValid(KeyCode input)
-    {
-        switch (input)
-        {
-            case KeyCode.W:
-                return (totalLevelMap[currentX - 1, currentY] == 0 || totalLevelMap[currentX - 1, currentY] == 5 || totalLevelMap[currentX - 1, currentY] == 6);
-            case KeyCode.A:
-                return (totalLevelMap[currentX, currentY - 1] == 0 || totalLevelMap[currentX, currentY - 1] == 5 || totalLevelMap[currentX, currentY - 1] == 6);
-            case KeyCode.S:
-                return (totalLevelMap[currentX + 1, currentY] == 0 || totalLevelMap[currentX + 1, currentY] == 5 || totalLevelMap[currentX + 1, currentY] == 6);
-            case KeyCode.D:
-                return (totalLevelMap[currentX, currentY + 1] == 0 || totalLevelMap[currentX, currentY + 1] == 5 || totalLevelMap[currentX, currentY + 1] == 6);
-            default:
-                return false;
-        }
-    }*/
 
-    /*private void PlayMoveAnimation()
+
+    private void PlayMoveAnimation()
     {
+
         Vector3 currentPosition = pacStudent.transform.position;
         Vector3 direction = (currentPosition - lastPosition).normalized;
 
@@ -170,53 +145,27 @@ public class PacStudentController : MonoBehaviour
         pacStudentAnimator.SetBool("PacStudent_Left", false);
         pacStudentAnimator.SetBool("PacStudent_Down", false);
 
-        switch (currentInput)
+        if (direction.x > 0)
         {
-            case KeyCode.W:
-                pacStudentAnimator.SetBool("PacStudent_Up", true);
-                break;
-            case KeyCode.A:
-                pacStudentAnimator.SetBool("PacStudent_Left", true);
-                break;
-            case KeyCode.S:
-                pacStudentAnimator.SetBool("PacStudent_Down", true);
-                break;
-            case KeyCode.D:
-                pacStudentAnimator.SetBool("PacStudent_Right", true);
-                break;
-            default:
-                break;
-
+            pacStudentAnimator.SetBool("PacStudent_Right", true);
         }
+        else if (direction.x < 0)
+        {
+            pacStudentAnimator.SetBool("PacStudent_Left", true);
+        }
+        else if (direction.y > 0)
+        {
+            pacStudentAnimator.SetBool("PacStudent_Up", true);
+        }
+        else if (direction.y < 0)
+        {
+            pacStudentAnimator.SetBool("PacStudent_Down", true);
+        }
+
+
         lastPosition = currentPosition;
-    }*/
-
-    private int[,] GenerateFullGrid(int[,] levelMap)
-    {
-        int rows = levelMap.GetLength(0);
-        int cols = levelMap.GetLength(1);
-        int totalRows = rows * 2 - 1;
-        int totalCols = cols * 2;
-
-        int[,] totalLevelMap = new int[totalRows, totalCols];
-
-        for (int x = 0; x < rows; x++)
-        {
-            for (int y = 0; y < cols; y++)
-            {
-                int num = levelMap[x, y];
-
-                totalLevelMap[x, y] = num;
-                totalLevelMap[x, totalCols - 1 - y] = num;
-
-                if (x != totalCols - 1)
-                {
-                    totalLevelMap[totalRows - 1 - x, y] = num;
-                    totalLevelMap[totalRows - 1 - x, totalCols - 1 - y] = num;
-                }
-            }
-        }
-        return totalLevelMap;
     }
+
+
 
 }
